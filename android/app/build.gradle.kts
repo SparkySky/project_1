cbb911cfb323ebd8876b277c6034167f39ad5efd
Alvin <alvinjiahao203@gmail.com>
2025-10-21 01:47:15 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,5 +73,4 @@ dependencies {
     implementation("com.huawei.hms:push:6.13.0.300")
     implementation("com.huawei.hms:hwid:6.12.0.300")
     implementation("com.huawei.hms:drive:5.2.0.300")
-    implementation("com.huawei.hms:ml-computer-rtt:3.2.0.300")
 }
