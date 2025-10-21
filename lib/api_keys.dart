e5dd96c2ec210ceb9899122c849d4912a8a7ecba
SparkySky <97169893+SparkySky@users.noreply.github.com>
2025-10-22 06:23:30 +0800

    Update

--- lib/api_keys.dart
+++ lib/api_keys.dart
@@ -1,5 +1,5 @@
 // .gitignore already
 const String GEMINI_API_KEY = String.fromEnvironment(
   'Backupkeyhere',
-  defaultValue: 'AIzaSyAv4eAJ5ycgrXPD9Ke0q4aix6z2T2N4zkM', // A fallback, but the goal is to always use the --dart-define flag.
+  defaultValue: '', // A fallback, but the goal is to always use the --dart-define flag.
 );
